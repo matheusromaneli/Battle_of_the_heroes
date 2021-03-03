d97f3224443bbd13eb7f52ac31695a7511bdcc36
Matheus Lima Romaneli <matheusromaneli@id.uff.br>
2021-03-03 20:58:08 -0300

    gravidade adimencional

--- main.py
+++ main.py
@@ -6,25 +6,34 @@ from PPlay.collision import *
 
 
 janela = Window(1280,720)
-velX = 250
-velY = 250
 janela.set_title("Battle of the heroes")
 teclado = Window.get_keyboard()
 jogador1 = Sprite("Assets/bola2.png", 1)
-
+jogador1.set_position(20,janela.height - jogador1.height - 50)
+jump1 = True
 fundo = GameImage("Assets/torcida2.jpg")
-
+velX = 250
+velY = 0
 while True:
+    jogador1.y -= velY * janela.delta_time()
+    if(jogador1.y > janela.height - jogador1.height - 50):
+        jump1 = True
+    
+    else:    
+        velY-= 600 * janela.delta_time()
+    print(velY)
+    if teclado.key_pressed("W"):
+        if(jump1):
+            velY = 250
+        jump1 = False
 
-    if teclado.key_pressed("W") and jogador1.y > 0:
-        jogador1.y -= velY * janela.delta_time()
-    if teclado.key_pressed("S") and jogador1.y < janela.height - jogador1.height - 50:
-        jogador1.y += velY * janela.delta_time()
     if teclado.key_pressed("A") and jogador1.x > 0:
         jogador1.x -= velX * janela.delta_time()
     if teclado.key_pressed("D") and jogador1.x < janela.width - jogador1.width:
         jogador1.x += velX * janela.delta_time()
     
+    # jogador1.y += 1
+
     fundo.draw()
     jogador1.draw()
     janela.update()
