1658ba583b774e2d9821fbebb78e3fb6d5a48729
Matheus Lima Romaneli <matheusromaneli@id.uff.br>
2021-03-03 21:18:44 -0300

    gambiarra tirada

--- main.py
+++ main.py
@@ -12,23 +12,25 @@ jogador1 = Sprite("Assets/bola2.png", 1)
 jogador1.set_position(20,janela.height - jogador1.height - 50)
 jump1 = True
 fundo = GameImage("Assets/torcida2.jpg")
-velX = 250
+velX = 450
 velY = 0
 while True:
 
-    jogador1.y -= velY * janela.delta_time()
-    if(jogador1.y > janela.height - jogador1.height - 50):
-        jump1 = True
-        jogador1.y += velY * janela.delta_time()
+    if jogador1.y > janela.height - jogador1.height - 50:
+        jump1 = True  
     
-    else:    
+    else:
+        jogador1.y -= velY * janela.delta_time()   
         velY-= 1600 * janela.delta_time()
-    print(velY)
+
     if teclado.key_pressed("W"):
         if(jump1):
             velY = 1000
+            jogador1.y -= velY * janela.delta_time()
         jump1 = False
 
+    if teclado.key_pressed("S"):
+        velY-= 1600 * janela.delta_time()
     if teclado.key_pressed("A") and jogador1.x > 0:
         jogador1.x -= velX * janela.delta_time()
     if teclado.key_pressed("D") and jogador1.x < janela.width - jogador1.width:
