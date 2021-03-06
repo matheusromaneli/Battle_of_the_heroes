b0a31a6ea24bae2e39614774736136620917c384
Matheus Lima Romaneli <matheusromaneli@id.uff.br>
2021-03-06 10:03:42 -0300

    animaçao movimento dos 2 players

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ class Main():
         for i in jogador1:
             jogador1[i].set_position(20,janela.height - jogador1[i].height - 30)
             jogador1[i].set_total_duration(1000)
-        # jogador2 = Sprite("Assets/Gladiador2-corrida-esquerda.png", 8)
-        # jogador2.set_position(1140,janela.height - jogador2.height - 30)
+        jogador2 = {'direita' : Sprite("Assets/Gladiator2-corrida.png", 8), 'esquerda' :Sprite("Assets/Gladiator2-corrida-esquerda.png", 8), 'parado' :Sprite("Assets/Gladiator-parado.png",5)}
+        for i in jogador2:
+            jogador2[i].set_position(janela.width-jogador2[i].width-20,janela.height - jogador2[i].height - 30)
+            jogador2[i].set_total_duration(1000)
         jump1 = True
         jump2 = True
         fundo = GameImage("Assets/torcida2.jpg")
@@ -26,13 +28,14 @@ class Main():
         while True:
 
             velY ,jump1 = Jogador.controles(jogador1, janela, velX, velY, teclado, jump1, "w","s","a","d")
-            # velX2, velY2 ,jump2 = Jogador.controles(jogador2, janela, velX2, velY2, teclado, jump2, "up","down","left","right")
+            velY2 ,jump2 = Jogador.controles(jogador2, janela, velX2, velY2, teclado, jump2, "up","down","left","right")
 
             
             fundo.draw()
             for i in jogador1:
                 jogador1[i].draw()
                 jogador1[i].update()
-            # jogador2.draw()
-            # jogador2.update()
+            for i in jogador2:
+                jogador2[i].draw()
+                jogador2[i].update()
             janela.update()
