279ada294ca43560e8230c008f463eb75fd15776
leosaracino <leonardosaracino@id.uff.br>
2021-03-29 11:44:05 -0300

    finalizando plaraformas

--- jogador.py
+++ jogador.py
@@ -20,8 +20,7 @@ def set_animation(direita,esquerda,ataqueDireita, ataqueEsquerda, jogador):
 class Jogador():
 
     @classmethod
-    def controles(self,jogador, janela, velX, velY, teclado, jump, a, b, c, d, e, plataformas):
-
+    def controles(self,jogador, janela, velX, velY, teclado, jump, a, b, c, d, e, plataformas, pe, velpeX, velpeY):
         posInicial = jogador['direita'].x
         last = ''
         ##Física do pulo
@@ -32,11 +31,14 @@ class Jogador():
         else:
             for i in jogador:
                 jogador[i].y -= velY * janela.delta_time()
+                pe.y -= velpeY * janela.delta_time()
             velY-= 2000 * janela.delta_time()
-
+            velpeY -= 400 * janela.delta_time()
+           
             for plataforma in plataformas:
-                if Collision.collided(jogador['direita'], plataforma) and velY < 0:   
+                if Collision.collided(pe, plataforma) and velY < 0:   
                     velY = 0
+                    velpeY = 0
                     jump = True
 
         ##Controles
@@ -45,19 +47,23 @@ class Jogador():
             if(jump):
 
                 velY = 1000
+                velpeY = 200
                 for i in jogador:
                     jogador[i].y -= velY * janela.delta_time()
+                    pe.y -= velpeY * janela.delta_time()
 
             jump = False
 
         if teclado.key_pressed(b):
-
             velY-= 2000 * janela.delta_time()
+            velpeY -= 400 * janela.delta_time()
+            
         if teclado.key_pressed(c) and jogador['direita'].x > 0:
             last = 'direita'
 
             for i in jogador:
                 jogador[i].x -= velX * janela.delta_time()
+                pe.x -= velpeX * janela.delta_time()
     
             set_animation(0,1,0,0,jogador)
 
@@ -69,6 +75,7 @@ class Jogador():
 
             for i in jogador:
                 jogador[i].x += velX * janela.delta_time()
+                pe.x += velpeX * janela.delta_time()
 
             set_animation(1,0,0,0,jogador)
 
@@ -83,4 +90,4 @@ class Jogador():
         # else:
         #     if teclado.key_pressed(e):
         #         set_animation(0,0,1,0,jogador)
-        return velY , jump
+        return velY , jump , velpeY
--- jogo.py
+++ jogo.py
@@ -6,11 +6,11 @@ from PPlay.collision import *
 from jogador import *
 from PPlay.animation import *
 
-class Main():
+class Jogo():
     janela = Window(1280,720)
     janela.set_title("Battle of the heroes")
     teclado = Window.get_keyboard()
-    jogador1 = {'direita' : Sprite("Assets/Jogador1/corrida-direita1.png", 8), 'esquerda' :Sprite("Assets/Jogador1/corrida-esquerda1.png", 8), 'parado' :Sprite("Assets/Jogador1/parado1.png",5), 'ataque-direita' :Sprite("Assets/Jogador1/Ataque-direita1.png", 7), 'ataque-esquerda' :Sprite("Assets/Jogador1/Ataque-esquerda1.png", 7)}
+    jogador1 = {'direita' : Sprite("Assets/Jogador1/corrida-direita1.png", 8), 'esquerda' :Sprite("Assets/Jogador1/corrida-esquerda1.png", 8), 'parado' :Sprite("Assets/Jogador1/parado1.png",5), 'ataque-direita' :Sprite("Assets/Jogador1/ataque-direita-azul (1).png", 7), 'ataque-esquerda' :Sprite("Assets/Jogador1/Ataque-esquerda1.png", 7)}
     for i in jogador1:
         jogador1[i].set_position(20,janela.height - jogador1[i].height - 30)
         jogador1[i].set_total_duration(1000)
@@ -18,6 +18,10 @@ class Main():
     for i in jogador2:
         jogador2[i].set_position(janela.width-jogador2[i].width-20,janela.height - jogador2[i].height - 30)
         jogador2[i].set_total_duration(1000)
+    pe = Sprite("Assets/pad2.png", 1)
+    pe2 = Sprite("Assets/pad2.png", 1)
+    pe.set_position(66,janela.height - pe.height - 33)
+    pe2.set_position(janela.width-pe.width-66,janela.height - pe.height - 45)
     jump1 = True
     jump2 = True
     fundo = GameImage("Assets/torcida2.jpg")
@@ -25,18 +29,37 @@ class Main():
     velY = 0
     velX2= 450
     velY2 = 0
+    velpeX = 90
+    velpeY = 0
+    velpeX2 = 90
+    velpeY2 = 0
     plataforma = GameImage("Assets/plataformaPequena.png")
     plataforma2 = GameImage("Assets/plataformaMedia.png")
     plataforma3 = GameImage("Assets/plataformaPequena.png")
     plataforma.set_position(150, janela.height - plataforma.height- 175)
     plataforma2.set_position(640 - (plataforma2.width/2),janela.height - plataforma2.height - 325)
     plataforma3.set_position(janela.width - 320, janela.height - plataforma3.height - 175)
+    chao1 = GameImage("Assets/chao-plataformaPequena.png")
+    chao2 = GameImage("Assets/chao-plataformaPequena.png")
+    chao3 = GameImage("Assets/chao-plataformaMedia.png")
+    chao1.set_position(150,janela.height - plataforma.height- 175)
+    chao2.set_position(janela.width - 320, janela.height - plataforma.height- 175)
+    chao3.set_position(640 - (plataforma2.width/2),janela.height - plataforma2.height - 325)
+    cont = 0
+    fps = 0
+    atual = 0
     while True:
 
-        velY ,jump1 = Jogador.controles(jogador1, janela, velX, velY, teclado, jump1, "w","s","a","d","space", [plataforma , plataforma2 , plataforma3])
-        velY2 ,jump2 = Jogador.controles(jogador2, janela, velX2, velY2, teclado, jump2, "up","down","left","right","enter",[plataforma, plataforma2 , plataforma3])
-
+        velY ,jump1, velpeY= Jogador.controles(jogador1, janela, velX, velY, teclado, jump1, "w","s","a","d","space", [chao1, chao2, chao3],pe,velpeX,velpeY)
+        velY2 ,jump2, velpeY2= Jogador.controles(jogador2, janela, velX2, velY2, teclado, jump2, "up","down","left","right","enter",[chao1, chao2, chao3],pe2,velpeX2,velpeY2)
+        cont += janela.delta_time()
+        fps += 1
+        if cont>1:
+            atual= fps
+            cont=0
+            fps=0
         fundo.draw()
+        janela.draw_text(f"FPS:{atual}", 10, 10, size=40, color=(255,255,255), font_name= 'Segoe UI', bold=False, italic=False)
         plataforma.draw()
         plataforma2.draw()
         plataforma3.draw()
@@ -46,4 +69,5 @@ class Main():
         for i in jogador2:
             jogador2[i].draw()
             jogador2[i].update()
+        
         janela.update()
