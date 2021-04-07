117e8ce83e65ff304a9d25858f9e6191746a6ce4
leosaracino <leonardosaracino@id.uff.br>
2021-04-07 12:44:14 -0300

    adicionando menu e creditos

--- creditos.py
+++ creditos.py
@@ -6,14 +6,11 @@ class Creditos:
         janela = Window(1200,720)
         janela.set_title("Creditos")
         janela.set_background_color ((0,0,0))
-        # fundo = GameImage("Assets/Creditos.jpg")
         texto = "   Obrigado por jogar Battle of the Heroes!"
         texto2 = "Esse jogo foi desenvolvido a fins educacionais por"
         texto3 = "Leonardo Saracino e Matheus Romaneli."
         while True:
-
-
-            # fundo.draw()
+  
             janela.draw_text(texto,150, 80, size=50, color=(255,255,255), font_name= 'Segoe UI', bold=False, italic=False)
             janela.draw_text(texto2,80,300, size=50, color=(255,255,255), font_name= 'Segoe UI', bold=False, italic=False)
             janela.draw_text(texto3,200, 380, size=50, color=(255,255,255), font_name= 'Segoe UI', bold=False, italic=False)
